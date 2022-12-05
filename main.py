05a313809e666454114f27e8aefb460e0ec4dd61
precupa-2022 <precupa@educ.college-montreal.qc.ca>
2022-12-04 19:31:51 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Programme fait par Alexander Precup
+Groupe: 402
+Description: Objets orientées
+Exercise 1 - Écrire une classe StringFoo avec un attribut message et deux méthodes set_string et print_string
+"""
+
+class StringFoo:
+    message = ""
+
+    def set_string(self,message):
+        self.message = input("Entrez un mot ou une phrase : ")
+
+    def print_string(self,message):
+        self.message = message
+        print(message.upper())
+
+s = StringFoo()
+s.set_string("avion")
+s.print_string(s.message)
