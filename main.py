5571cab7c4d4d95d04b936a5a36803e583d22896
precupa-2022 <precupa@educ.college-montreal.qc.ca>
2022-12-19 20:27:37 -0500

    Correction erreurs PEP8

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ Description: Objets orientées
 Exercise 1 - Écrire une classe StringFoo avec un attribut message et deux méthodes set_string et print_string
 """
 
+
 class StringFoo:
     message = ""
 
-    def set_string(self,message):
+    def set_string(self):
         self.message = input("Entrez un mot ou une phrase : ")
 
-    def print_string(self,message):
+    def print_string(self, message):
         self.message = message
         print(message.upper())
 
+
 s = StringFoo()
-s.set_string(s.message)
+s.set_string()
 s.print_string(s.message)
